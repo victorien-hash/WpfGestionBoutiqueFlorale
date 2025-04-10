64b28802a7da32704705727ec378ba608059bfd6
victorien-hash <victorienfotso@gmail.com>
2025-04-10 17:12:44 -0400

    first commit

---
+++ WpfGestionBoutiqueFlorale/Views/ClientView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfGestionBoutiqueFlorale.Views
+{
+    public partial class ClientView : UserControl
+    {
+        public ClientView()
+        {
+            InitializeComponent();
+        }
+
+        private void AjouterClient_Click(object sender, RoutedEventArgs e)
+        {
+            string nom = NomClientTextBox.Text;
+            string email = EmailClientTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(nom) || string.IsNullOrWhiteSpace(email))
+            {
+                MessageBox.Show("Veuillez remplir tous les champs.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                MessageBox.Show($"Client ajouté : {nom} ({email})", "Succès", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+
+}
---
+++ WpfGestionBoutiqueFlorale/Views/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGestionBoutiqueFlorale.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void AjouterClientMenu_Click(object sender, RoutedEventArgs e)
+    {
+        // Vérifie si l'onglet existe déjà
+        foreach (TabItem tab in MainTabControl.Items)
+        {
+            if (tab.Header.ToString() == "Ajouter un client")
+            {
+                MainTabControl.SelectedItem = tab;
+                return;
+            }
+        }
+
+        // Crée le contrôle client
+        ClientView clientView = new ClientView();
+
+        // Crée un nouvel onglet
+        TabItem tabItem = new TabItem
+        {
+            Header = "Ajouter un client",
+            Content = clientView
+        };
+
+        MainTabControl.Items.Add(tabItem);
+        MainTabControl.SelectedItem = tabItem;
+    }
+}
