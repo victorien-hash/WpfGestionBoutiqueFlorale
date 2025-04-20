593c50c5db0d24c88d5e4da6f9f9c1ec67048e00
victorien-hash <victorienfotso@gmail.com>
2025-04-19 23:24:26 -0400

    annuler et valider facture ok

--- WpfGestionBoutiqueFlorale/ViewModels/VendeurViewModel.cs
+++ WpfGestionBoutiqueFlorale/ViewModels/VendeurViewModel.cs
@@ -1,12 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfGestionBoutiqueFlorale.Models;
 
 namespace WpfGestionBoutiqueFlorale.ViewModels
 {
-    class VendeurViewModel
+    class VendeurViewModel : ViewModelBase
     {
+        public ObservableCollection<Commande> CommandeDisponibles { get; set; }
+        public ObservableCollection<Utilisateur> VendeursDisponibles { get; set; }
+        public Commande CommandeSelectionnee { get; set; }
+        public Utilisateur VendeurSelectionnee { get; set; }
+
+        public string MoyenPaiement { get; set; }
+
+        public ICommand ValiderCommande { get; set; }
+        public ICommand AnnulerCommande { get; set; }
+        public ICommand GenererFacture { get; set; }
+
+
+        
+
+            public VendeurViewModel()
+            {
+                using var db = new GestionFloraleDbContext();
+
+                VendeursDisponibles = new ObservableCollection<Utilisateur>(db.Utilisateurs.Where(u => u.role == "vendeur").ToList());
+                CommandeDisponibles = new ObservableCollection<Commande>(db.Commandes.ToList());
+
+
+                ValiderCommande = new RelayCommand(ExecuterValiderCommande);
+                AnnulerCommande = new RelayCommand(ExecuterAnnulerCommande);
+                GenererFacture = new RelayCommand(ExecuterGenererFacture);
+                
+            }
+
+        public void ExecuterValiderCommande()
+        {
+            if (CommandeSelectionnee != null)
+            {
+                using (var db = new GestionFloraleDbContext())
+                {
+                    var commande = db.Commandes.FirstOrDefault(c => c.IdCommande == CommandeSelectionnee.IdCommande);
+
+                    if (commande != null)
+                    {
+                        commande.EstValidee = true;
+                        db.SaveChanges();
+
+                        // Mettre à jour la liste locale pour refléter le changement (facultatif selon ton affichage)
+                        CommandeSelectionnee.EstValidee = true;
+                    }
+                }
+                MessageBox.Show("Votre commande a bien été validée.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            else
+            {
+                // Tu peux aussi afficher un message si tu as une gestion d'erreurs ou de notifications
+                MessageBox.Show("Veuillez sélectionner une commande à valider.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        public void ExecuterAnnulerCommande()
+        {
+            if (CommandeSelectionnee != null)
+            {
+                using (var db = new GestionFloraleDbContext())
+                {
+                    var commande = db.Commandes.FirstOrDefault(c => c.IdCommande == CommandeSelectionnee.IdCommande);
+
+                    if (commande != null)
+                    {
+                        commande.EstValidee = false;
+                        db.SaveChanges();
+
+                        // Mise à jour locale
+                        CommandeSelectionnee.EstValidee = false;
+                    }
+
+                }
+
+                MessageBox.Show("Votre commande a bien été annulée.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner une commande à annuler.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        public void ExecuterGenererFacture() { }
+        //{
+        //    if (CommandeSelectionnee == null || VendeurSelectionnee == null || string.IsNullOrWhiteSpace(MoyenPaiement))
+        //    {
+        //        MessageBox.Show("Veuillez sélectionner une commande, un vendeur et un moyen de paiement.",
+        //                        "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+        //        return;
+        //    }
+
+        //    using (var context = new GestionFloraleDbContext())
+        //    {
+        //        var facture = new Facture
+        //        {
+        //            IdCommande = CommandeSelectionnee.IdCommande,
+        //            IdVendeur = VendeurSelectionnee.IdUtilisateur,
+        //            MoyenPaiement = MoyenPaiement,
+                    
+        //        };
+
+        //        context.Factures.Add(facture);
+        //        context.SaveChanges();
+        //    }
+
+        //    MessageBox.Show("Facture générée avec succès !", "Succès", MessageBoxButton.OK, MessageBoxImage.Information);
+        //}
+
+
     }
 }
--- WpfGestionBoutiqueFlorale/Views/BouquetView.xaml.cs
+++ WpfGestionBoutiqueFlorale/Views/BouquetView.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfGestionBoutiqueFlorale.ViewModels;
 
 namespace WpfGestionBoutiqueFlorale.Views
 {
@@ -22,6 +23,7 @@ namespace WpfGestionBoutiqueFlorale.Views
         public BouquetView()
         {
             InitializeComponent();
+            DataContext = new BouquetViewModel();
         }
     }
 }
